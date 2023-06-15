96f0a75ef0c884bc640909144a11ee2e1811b213
dw <dsw6ru@virginia.edu>
2023-06-15 18:40:16 -0400

    initial viz

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import AudioSource from './audio/AudioSource';
 import AudioAnalyzer from './audio/AudioAnalyzer';
+import * as d3 from 'd3';
 
-function App() {
+const App = () => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const audioSourceRef = useRef<AudioSource | null>(null);
   const analyzerRef = useRef<AudioAnalyzer | null>(null);
+  const svgRef = useRef<SVGSVGElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const audioSource = new AudioSource("/Ember-Kubbi.mp3");
+    const audioElement = audioSource.getAudioElement();
+    audioElement.crossOrigin = "anonymous";
+
+    audioSourceRef.current = audioSource;
+    audioRef.current = audioElement;
+  }, []);
 
   const playAudio = () => {
     setIsPlaying(true);
+
     if (audioSourceRef.current && !audioSourceRef.current.getAudioContext()) {
       audioSourceRef.current.initializeAudio();
       analyzerRef.current = new AudioAnalyzer(audioSourceRef.current);
@@ -19,12 +29,11 @@ function App() {
 
     if (audioRef.current) {
       audioRef.current.play();
-
-      // Analyze audio data every 100ms
+      // Visualize audio data every 100ms
       const intervalId = setInterval(() => {
         if (analyzerRef.current) {
           const frequencyData = analyzerRef.current.getFrequencyData();
-          console.log(frequencyData);
+          visualizeAudioData(frequencyData);
         }
       }, 100);
 
@@ -42,30 +51,37 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    const audioSource = new AudioSource("/Ember-Kubbi.mp3");
-    const audioElement = audioSource.getAudioElement();
-    audioElement.crossOrigin = "anonymous";
+  const visualizeAudioData = (frequencyData: Uint8Array) => {
+    const svg = d3.select(svgRef.current);
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const width = +svg.attr('width')! - margin.left - margin.right;
+    const height = +svg.attr('height')! - margin.top - margin.bottom;
+    const barWidth = Math.ceil(width / frequencyData.length);
 
-    audioSourceRef.current = audioSource;
-    audioRef.current = audioElement;
-  }, []);
+    const x = d3.scaleLinear().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    svg.selectAll("*").remove();  // Clear the SVG
+
+    const g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    x.domain([0, frequencyData.length]);
+    y.domain([0, d3.max(frequencyData)!]);
+
+    g.selectAll('.bar')
+        .data(frequencyData)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', (d, i) => x(i)!)
+        .attr('width', barWidth)
+        .attr('y', d => y(d)!)
+        .attr('height', d => height - y(d)!);
+};
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <svg ref={svgRef} width="800" height="400" />
         <button onClick={() => isPlaying ? pauseAudio() : playAudio()}>
           {isPlaying ? 'Pause' : 'Play'}
         </button>
